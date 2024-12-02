b6cd61c896d57795bd6f0e14686652ef26082b67
Sebastian Raaphorst <sraaphorst@gmail.com>
2024-12-02 09:42:05 -0500

    Added day 2.

---
+++ src/test/kotlin/day02/day02.kt
@@ -0,0 +1,32 @@
+// Advent of Code 2024, Day 02.
+// By Sebastian Raaphorst, 2024.
+
+package day02
+
+import common.parseGrid
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day01Test {
+    private companion object {
+        val reports = parseGrid(
+            """
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
+            """.trimIndent(), String::toInt)
+    }
+
+    @Test
+    fun `Problem 1 example`() {
+        assertEquals(2, answer1(reports))
+    }
+
+    @Test
+    fun `Problem 2 example`() {
+        assertEquals(4, answer2(reports))
+    }
+}
